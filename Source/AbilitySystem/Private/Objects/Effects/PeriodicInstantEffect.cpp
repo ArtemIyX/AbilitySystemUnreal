295a533b170eed25cafbac67c74232f1cb52c11c
Wellsaik <artem.podorozhko@outlook.com>
2024-10-20 22:55:20 +0300

    Periodic instant effect

---
+++ Source/AbilitySystem/Private/Objects/Effects/PeriodicInstantEffect.cpp
@@ -0,0 +1,33 @@
+﻿// © Artem Podorozhko. All Rights Reserved. This project, including all associated assets, code, and content, is the property of Artem Podorozhko. Unauthorized use, distribution, or modification is strictly prohibited.
+
+
+#include "Objects/Effects/PeriodicInstantEffect.h"
+
+UPeriodicInstantEffect::UPeriodicInstantEffect(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer),
+	Period(1.0f)
+{
+}
+
+void UPeriodicInstantEffect::RunPeriodicTimer()
+{
+	FTimerManager& manager = GetWorld()->GetTimerManager();
+	if (manager.IsTimerActive(PeriodicTimerHandle))
+		manager.ClearTimer(PeriodicTimerHandle);
+	manager.SetTimer(PeriodicTimerHandle, FTimerDelegate::CreateUObject(this, &UPeriodicInstantEffect::PeriodTick),
+	                 FMath::IsNearlyZero(Period) ? 0.1f : Period, true);
+}
+
+void UPeriodicInstantEffect::StartWork_Implementation()
+{
+	Super::StartWork_Implementation();
+	RunPeriodicTimer();
+}
+
+FString UPeriodicInstantEffect::GetDebugString_Implementation() const
+{
+	return FString(TEXT("PeriodicInstantEffect"));
+}
+
+void UPeriodicInstantEffect::PeriodTick_Implementation()
+{
+}
---
+++ Source/AbilitySystem/Public/Objects/Effects/PeriodicInstantEffect.h
@@ -0,0 +1,59 @@
+﻿// © Artem Podorozhko. All Rights Reserved. This project, including all associated assets, code, and content, is the property of Artem Podorozhko. Unauthorized use, distribution, or modification is strictly prohibited.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Effect.h"
+#include "PeriodicInstantEffect.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class ABILITYSYSTEM_API UPeriodicInstantEffect : public UEffect
+{
+	GENERATED_BODY()
+
+public:
+	UPeriodicInstantEffect(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+protected:
+	/**
+	* @brief Starts a timer that performs an side action based on the specified duration.
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly)
+	virtual void RunPeriodicTimer();
+
+	/**
+	 * @brief Called periodically
+	 *
+	 * The `PeriodTick` function is executed at regular intervals while the effect is active. It allows the developer
+	 * to define custom logic that should occur periodically, such as applying damage, healing, or other time-based effects.
+	 * This function is intended to be overridden in derived classes for specific behavior.
+	 */
+	UFUNCTION(BlueprintNativeEvent)
+	void PeriodTick();
+public:
+
+	virtual void StartWork_Implementation() override;
+	virtual FString GetDebugString_Implementation() const override;
+
+	/**
+	* @brief The period of the side effect in seconds.
+	*
+	* @note The minimum value is set to 0.0001 seconds.
+	*/
+	UPROPERTY(BlueprintReadWrite,
+		EditDefaultsOnly,
+		meta=(UIMin="0.0001",
+			ClampMin="0.0001"),
+		Category="PeriodicEffect")
+	float Period;
+
+	
+	/**
+	* @brief Handle for managing the periodic timer.
+	*
+	* This property is read-only and is used internally to track the timer's state.
+	*/
+	FTimerHandle PeriodicTimerHandle;
+};
